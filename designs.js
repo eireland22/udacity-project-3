09e8332d37985d8a7ef5f1a1d10fe8a73a9b9ab8
eireland22 <eireland22@gmail.com>
2021-05-19 05:03:10 -0500

    pixel art maker - udacity project 3

---
+++ designs.js
@@ -0,0 +1,36 @@
+// Select color input
+// Select size input
+var height, width, color;
+
+// When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function (event)) {
+    event.preventDefault();
+    height = $('#inputHeight').val();
+    width = $('#inputWidth').val();
+    makeGrid(height, width); 
+    // console.log('Height: ' + height + 'and width:' + width);
+}
+
+
+function makeGrid(n, m) {
+    $('tr').remove();
+
+    // Your code goes here!
+    for (var i = 1; i <= n; i++) {
+        $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+        for (var j = 1; j <= m; j++) {
+            $('#table' + i).append('<td></td>');
+        }
+    }
+
+    // add some color to each grid square
+    $('td').click(function addColor() {
+        color = $('#colorPicker').val();
+
+        if ($(this).attr('style')) {
+            $(this).removeAttr('style')
+        } else {
+            $(this).attr('style', 'background-color:' + color);
+        }
+    }
+}

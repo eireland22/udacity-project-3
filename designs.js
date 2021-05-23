d83442423e115df23eb8a36bf00973848aa9b8d4
eireland22 <eireland22@gmail.com>
2021-05-23 09:01:19 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -8,11 +8,11 @@ function makeGrid(height, width) {
     let grid = '';
     
     // Looping over row
-    for (let n = 0; n < height; n++) {
-        grid += '<tr class="row-' + n + '">';
+    for (let i = 0; i < height; i++) {
+        grid += '<tr class="row-' + i + '">';
         // Looping over cell
-        for (let m = 0; m < width; m++) {
-            grid += '<td class="cell" id="row-' + n + '_cell-' + m + '"></td>';
+        for (let j = 0; j < width; j++) {
+            grid += '<td class="cell" id="row-' + i + '_cell-' + j + '"></td>';
         }
         grid += '</tr>';
     }
@@ -23,3 +23,22 @@ function makeGrid(height, width) {
     // Adding click event to grid
     addClickEventToCells();
 }
+
+function formSubmission() {
+    event.preventDefault();
+    constr height = document.getElementById('input_height').value;
+    constr width = document.getElementById('input_width').value;
+    makeGrid(height, width);
+}
+
+function addClickEventToCells() {
+    constr colorPicker = document.getElementById("colorPicker");
+    constr cells = document.getElementByClassName('cell');
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener("click", function (event) {
+            let clickedCell = event.target;
+            clickedcell.style.backgroundColor = colorPicker.value;
+        });
+    }
+}
+
